623701b62f519adb0bb85fac52f9515aaff49832
Vadim Vaskevich <vadskev@ya.ru>
2024-04-02 23:05:33 +0300

    add posttask.go

--- internal/handler/posttask/posttask.go
+++ internal/handler/posttask/posttask.go
@@ -1,13 +1,52 @@
 package posttask
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"net/http"
 
-	"github.com/vadskev/go-rest-api-homework/internal/storage/memstorage"
+	"github.com/vadskev/go-rest-api-homework/internal/model"
 )
 
-func New(store *memstorage.MemStorage) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+var (
+	ErrMethodRequest       = errors.New("POST Is not POST Request")
+	ErrFailedDecodeJson    = errors.New("POST failed to decode json")
+	ErrFailedDecodeRequest = errors.New("POST failed to decode request body")
+)
 
+type TaskStore interface {
+	Add(string, model.Task) error
+}
+
+func New(store TaskStore) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var task model.Task
+		var buf bytes.Buffer
+
+		if r.Method != http.MethodPost {
+			http.Error(w, ErrMethodRequest.Error(), http.StatusBadRequest)
+			return
+		}
+
+		_, err := buf.ReadFrom(r.Body)
+		if err != nil {
+			http.Error(w, ErrFailedDecodeRequest.Error(), http.StatusBadRequest)
+			return
+		}
+
+		if err = json.Unmarshal(buf.Bytes(), &task); err != nil {
+			http.Error(w, ErrFailedDecodeJson.Error(), http.StatusBadRequest)
+			return
+		}
+
+		err = store.Add(task.ID, task)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
 	}
 }
