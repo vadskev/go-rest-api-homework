525fb665c789cdeab51a424422dc6645502a6e98
Vadim Vaskevich <vadskev@ya.ru>
2024-04-02 23:05:39 +0300

    add storage.go

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -4,8 +4,8 @@ import "github.com/vadskev/go-rest-api-homework/internal/model"
 
 // Storage
 type Storage interface {
-	GetAll() (*map[string]model.Task, error)
-	Add(string, *model.Task) error
+	GetAll() (map[string]model.Task, error)
+	Add(string, model.Task) error
 	Get(key string) (model.Task, error)
-	Delete(key string) (model.Task, error)
+	Delete(key string) error
 }
