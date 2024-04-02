445349d785b418a7b41f196b2b082116f586405a
Vadim Vaskevich <vadskev@ya.ru>
2024-04-02 23:04:20 +0300

    add deletetask.go

--- internal/handler/deletetask/deletetask.go
+++ internal/handler/deletetask/deletetask.go
@@ -1,13 +1,36 @@
 package deletetask
 
 import (
+	"errors"
 	"net/http"
 
-	"github.com/vadskev/go-rest-api-homework/internal/storage/memstorage"
+	"github.com/go-chi/chi/v5"
 )
 
-func New(store *memstorage.MemStorage) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+var (
+	ErrMethodRequest = errors.New("DELETE Is not DELETE Request")
+)
 
+type TaskStore interface {
+	Delete(string) error
+}
+
+func New(store TaskStore) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodDelete {
+			http.Error(w, ErrMethodRequest.Error(), http.StatusBadRequest)
+			return
+		}
+
+		idTask := chi.URLParam(r, "id")
+
+		err := store.Delete(idTask)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
 	}
 }
