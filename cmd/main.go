6643ae03d880792b0b2d790a169812ae885c9bbc
Vadim Vaskevich <vadskev@ya.ru>
2024-04-02 23:05:10 +0300

    fix main.go

--- cmd/main.go
+++ cmd/main.go
@@ -9,21 +9,7 @@ import (
 )
 
 func main() {
-
 	store := memstorage.InitStorage()
-
-	/*
-		item := model.Task{
-			ID:          "1",
-			Description: "sdfsdf",
-			Note:        "Еyuiyui",
-			Application: []string{
-				"fgh",
-				"uium",
-				"456rth",
-			},
-		}
-	*/
 
 	if err := http.ListenAndServe(":8080", router.New(store)); err != nil {
 		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
